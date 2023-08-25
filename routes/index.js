e9018b03d4686d5516dac261e52ccdc32b36a078
BuyanauskasAA <Buyanauskas@gmail.com>
2023-08-25 20:35:15 +0600

    fix: обработка неправильного URL

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
 router.use('*', (req, res) => {
   res.status(404).send({ message: 'Wrong URL request to server' });
 });
-
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
 
 module.exports = router;
