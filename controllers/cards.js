ea3a77bff1b837b53f18af1e479c97ae7c172dea
BuyanauskasAA <Buyanauskas@gmail.com>
2023-08-25 18:56:50 +0600

    feat: написать controllers

---
+++ controllers/cards.js
@@ -0,0 +1,95 @@
+const Card = require('../models/card');
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner: req.user._id })
+    .then((card) => res.status(201).send(card))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const getCards = (req, res) => {
+  Card.find()
+    .populate(['owner', 'likes'])
+    .then((cards) => res.status(200).send(cards))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const getCardById = (req, res) => {
+  Card.findById(req.params.cardId)
+    .populate(['owner', 'likes'])
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Card not found' });
+        return;
+      }
+
+      res.status(200).send(card);
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .populate(['owner', 'likes'])
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Card not found' });
+        return;
+      }
+
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .populate(['owner', 'likes'])
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Card not found' });
+        return;
+      }
+
+      res.status(200).send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = {
+  createCard,
+  getCards,
+  getCardById,
+  likeCard,
+  dislikeCard,
+};
---
+++ controllers/users.js
@@ -0,0 +1,92 @@
+const User = require('../models/user');
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const getUsers = (req, res) => {
+  User.find()
+    .then((users) => res.status(200).send(users))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'User not found' });
+        return;
+      }
+
+      res.status(200).send(user);
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { ...req.body },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'User not found' });
+        return;
+      }
+
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
+};
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'User not found' });
+        return;
+      }
+
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: err.message });
+        return;
+      }
+
+      res.status(500).send({ message: err.message });
+    });
+};
+
+module.exports = {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  updateUserAvatar,
+};
