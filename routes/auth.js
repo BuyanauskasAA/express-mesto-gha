6709cf8d6f6c6455e1cbffaa237a72b68452f0f8
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-01 20:07:01 +0600

    fix: добавить поля валидации для регистрации

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,17 @@ router.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
+      name: Joi
+        .string()
+        .min(2)
+        .max(30),
+      about: Joi
+        .string()
+        .min(2)
+        .max(30),
+      avatar: Joi
+        .string
+        .uri(),
       email: Joi
         .string()
         .required()
