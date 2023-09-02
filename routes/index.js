4cf9b7d849aa070b6564a8f4bd6caf8a897af065
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-02 14:20:06 +0600

    fix: добавить класс ошибки UnauthorizedError

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,8 @@ const cardsRouter = require('./cards');
 const authRouter = require('./auth');
 
 router.use('/', authRouter);
-
-router.use(auth);
-
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', auth, usersRouter);
+router.use('/cards', auth, cardsRouter);
 
 router.use('*', (req, res) => {
   res.status(404).send({ message: 'Неверный URL запроса к серверу' });
