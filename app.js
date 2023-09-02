9eed8fc51e3514b19323297031bc35f1638bee10
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-03 02:52:52 +0600

    feat: добавить rate limit

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 mongoose
   .connect(DB_URL, {
@@ -21,6 +27,7 @@ mongoose
 const app = express();
 
 app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routes);
