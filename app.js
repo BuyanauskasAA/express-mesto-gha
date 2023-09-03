0dc7e4573bddd36b88c02a55ee563a034e189f61
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-03 11:59:41 +0600

    refactor: вынести rate-limiter в отдельный файл

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 
 const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
+const rateLimiter = require('./utils/rate-limiter');
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-});
 
 mongoose
   .connect(DB_URL, {
@@ -27,7 +22,7 @@ mongoose
 const app = express();
 
 app.use(helmet());
-app.use(limiter);
+app.use(rateLimiter);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routes);
